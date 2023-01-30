25670bea758f19f3a1e58e5720d44ce726c3040b
unknown <durveshrgawade@gmail.com>
2023-01-30 14:06:32 +0530

    Updates

---
+++ precache-manifest.9373363f5b4aa07360812dfb569ca44b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc45ff8d2aabbd85de55",
+    "url": "//tcetreactapp/static/css/main.8e083ce8.chunk.css"
+  },
+  {
+    "revision": "dc45ff8d2aabbd85de55",
+    "url": "//tcetreactapp/static/js/main.dc45ff8d.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "//tcetreactapp/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "//tcetreactapp/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "0545ef05dfe36adbece6",
+    "url": "//tcetreactapp/static/js/runtime~main.0545ef05.js"
+  },
+  {
+    "revision": "f6bb43240fd3de166190da1b7e0816a8",
+    "url": "//tcetreactapp/index.html"
+  }
+];
